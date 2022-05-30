b154217ba32d806dc8e524ff0bc41703aea85345
David Sharpe <david.a.sharpe@cgi.com>
2022-05-29 21:43:13 -0700

    Remove NEWCLIENT.

--- newclient.tf
+++ newclient.tf
@@ -1,23 +1,23 @@
-module "newclient" {
-  source = "./modules/payara-client"
-
-  client_id   = "NEWCLIENT"
-  claim_name  = "newclient_role"
-  base_url    = "https://cgi.com"
-  description = "New client description"
-  valid_redirect_uris = [
-    "http://localhost:8080/*",
-    "https://cgi.com/*",
-    "https://logontest7.gov.bc.ca/clp-cgi/logoff.cgi*",
-  ]
-  roles = {
-    "mapkey1" = {
-      "name"        = "ROLE1"
-      "description" = "A description for role 1"
-    },
-    "mapkey2" = {
-      "name"        = "ROLE2"
-      "description" = "A description for role 2"
-    }
-  }
-}
+#module "newclient" {
+#  source = "./modules/payara-client"
+#
+#  client_id   = "NEWCLIENT"
+#  claim_name  = "newclient_role"
+#  base_url    = "https://cgi.com"
+#  description = "New client description"
+#  valid_redirect_uris = [
+#    "http://localhost:8080/*",
+#    "https://cgi.com/*",
+#    "https://logontest7.gov.bc.ca/clp-cgi/logoff.cgi*",
+#  ]
+#  roles = {
+#    "ROLE1" = {
+#      "name"        = "ROLE1"
+#      "description" = "A description for role 1"
+#    },
+#    "ROLE1" = {
+#      "name"        = "ROLE2"
+#      "description" = "A description for role 2"
+#    }
+#  }
+#}
