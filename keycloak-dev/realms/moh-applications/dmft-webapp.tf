67a6844f188cd7b886bd0dda6a0cf84a3788c96b
David Sharpe <david.a.sharpe@cgi.com>
2022-06-30 09:45:33 -0700

    Create DMFT clients.

--- keycloak-dev/realms/moh-applications/dmft-webapp.tf
+++ keycloak-dev/realms/moh-applications/dmft-webapp.tf
@@ -1,15 +1,14 @@
 resource "keycloak_openid_client" "DMFT-WEBAPP" {
   realm_id                   = "moh_applications"
   client_id                  = "DMFT-WEBAPP"
+  name                       = "DMFT"
   enabled                    = true
   access_type                = "PUBLIC"
   standard_flow_enabled      = true
   full_scope_allowed         = false
   pkce_code_challenge_method = "S256"
   valid_redirect_uris        = [
-    "http://localhost:*",
-    "https://localhost:*",
-    "https://roadsafetybcportal-dev.apps.silver.devops.gov.bc.ca/rsbc-dfp-oidc/signin-oidc"
+    "*"
   ]
   web_origins = [
     "*"
