cb076dbd752b7a34e283766d541d06cd4f867db9
ahoplock <68440291+ahoplock@users.noreply.github.com>
2022-08-17 18:03:45 -0500

    Update main.tf
    view-client-hcimweb_hd2 isn't used with PIdP Dev

--- keycloak-dev/realms/moh_applications/pidp-service/main.tf
+++ keycloak-dev/realms/moh_applications/pidp-service/main.tf
@@ -82,7 +82,6 @@ module "scope-mappings" {
         "USER-MANAGEMENT-SERVICE/manage-user-details" = var.USER-MANAGEMENT-SERVICE.ROLES["manage-user-details"].id,
         "USER-MANAGEMENT-SERVICE/manage-user-roles" = var.USER-MANAGEMENT-SERVICE.ROLES["manage-user-roles"].id,
         "USER-MANAGEMENT-SERVICE/view-client-hcimweb" = var.USER-MANAGEMENT-SERVICE.ROLES["view-client-hcimweb"].id,
-        "USER-MANAGEMENT-SERVICE/view-client-hcimweb_hd2" = var.USER-MANAGEMENT-SERVICE.ROLES["view-client-hcimweb_hd2"].id,
         "USER-MANAGEMENT-SERVICE/view-client-pidp-service" = var.USER-MANAGEMENT-SERVICE.ROLES["view-client-pidp-service"].id,
         "USER-MANAGEMENT-SERVICE/view-client-sat-eforms" = var.USER-MANAGEMENT-SERVICE.ROLES["view-client-sat-eforms"].id,
         "USER-MANAGEMENT-SERVICE/view-client-uci-sso" = var.USER-MANAGEMENT-SERVICE.ROLES["view-client-uci-sso"].id,
@@ -114,10 +113,6 @@ module "service-account-roles" {
         "USER-MANAGEMENT-SERVICE/view-client-hcimweb" = {
             "client_id" = var.USER-MANAGEMENT-SERVICE.CLIENT.id,
             "role_id" = "view-client-hcimweb"
-        }
-        "USER-MANAGEMENT-SERVICE/view-client-hcimweb_hd2" = {
-            "client_id" = var.USER-MANAGEMENT-SERVICE.CLIENT.id,
-            "role_id" = "view-client-hcimweb_hd2"
         }
         "USER-MANAGEMENT-SERVICE/view-client-pidp-service" = {
             "client_id" = var.USER-MANAGEMENT-SERVICE.CLIENT.id,
