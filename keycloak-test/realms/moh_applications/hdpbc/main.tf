d89b18efa3c928e91c4cfe94966640883954af95
David Sharpe <david.a.sharpe@cgi.com>
2022-12-20 16:21:04 -0800

    Create HDPBC client in TEST. May be reused in DEV.

---
+++ keycloak-test/realms/moh_applications/hdpbc/main.tf
@@ -0,0 +1,51 @@
+resource "keycloak_openid_client" "CLIENT" {
+  access_token_lifespan               = ""
+  access_type                         = "CONFIDENTIAL"
+  backchannel_logout_session_required = true
+  base_url                            = "https://hdpbc-tst.hlth.gov.bc.ca/"
+  client_authenticator_type           = "client-secret"
+  client_id                           = "HDPBC"
+  consent_required                    = false
+  description                         = "Health Data Platform BC"
+  direct_access_grants_enabled        = false
+  enabled                             = true
+  frontchannel_logout_enabled         = false
+  full_scope_allowed                  = false
+  implicit_flow_enabled               = false
+  name                                = "HDPBC"
+  pkce_code_challenge_method          = ""
+  realm_id                            = "moh_applications"
+  service_accounts_enabled            = false
+  standard_flow_enabled               = true
+  use_refresh_tokens                  = true
+  valid_redirect_uris = [
+    "https://hdpbc-dev.hlth.gov.bc.ca/*",
+    "https://hdpbc-tst.hlth.gov.bc.ca/*",
+    "https://dmz1.hdpbc-tst.hlth.gov.bc.ca/*",
+    "https://dmz2.hdpbc-tst.hlth.gov.bc.ca/*",
+  ]
+  web_origins = [
+  ]
+}
+
+resource "keycloak_openid_client_default_scopes" "client_default_scopes" {
+  realm_id  = keycloak_openid_client.CLIENT.realm_id
+  client_id = keycloak_openid_client.CLIENT.id
+  default_scopes = [
+    "email",
+    "profile",
+    "roles",
+    "web-origins"
+  ]
+}
+
+resource "keycloak_openid_client_optional_scopes" "client_optional_scopes" {
+  realm_id  = keycloak_openid_client.CLIENT.realm_id
+  client_id = keycloak_openid_client.CLIENT.id
+  optional_scopes = [
+    "address",
+    "microprofile-jwt",
+    "offline_access",
+    "phone"
+  ]
+}
