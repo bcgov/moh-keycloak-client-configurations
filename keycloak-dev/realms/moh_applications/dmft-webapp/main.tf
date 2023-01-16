5101a45a40884bc231a713e3ad3519870d609a85
David Sharpe <david.a.sharpe@cgi.com>
2023-01-15 21:43:30 -0800

    Debugging.

--- keycloak-dev/realms/moh_applications/dmft-webapp/main.tf
+++ keycloak-dev/realms/moh_applications/dmft-webapp/main.tf
@@ -43,7 +43,7 @@ module "scope-mappings" {
   realm_id  = keycloak_openid_client.CLIENT.realm_id
   client_id = keycloak_openid_client.CLIENT.id
   roles = {
-    "LICENCE-STATUS/MOA"          = var.LICENCE-STATUS.ROLES["MOA"].id
+    "LICENCE-STATUS/MOA"          = var.LICENCE-STATUS.ROLES["MOA"].id,
     "LICENCE-STATUS/PRACTITIONER" = var.LICENCE-STATUS.ROLES["PRACTITIONER"].id
   }
 }
