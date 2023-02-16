f78765936e66bb55789159d8c15bce4aae9639cc
nathanielcx <nathanielc@uvic.ca>
2023-02-16 07:35:54 -0800

    Adding consumer role to PLR-SHOPPERS test client for messaging

--- keycloak-test/realms/moh_applications/plr-shoppers/main.tf
+++ keycloak-test/realms/moh_applications/plr-shoppers/main.tf
@@ -71,3 +71,11 @@ module "service-account-roles" {
   }
   client_roles = {}
 }
+module "scope-mappings" {
+  source    = "../../../../modules/scope-mappings"
+  realm_id  = keycloak_openid_client.CLIENT.realm_id
+  client_id = keycloak_openid_client.CLIENT.id
+  roles = {
+    "PLR_IAT/CONSUMER" = var.PLR_IAT.ROLES["CONSUMER"].id
+  }
+}
