b6dd9d65fe732b8fef99e3cf1d14d0881fa241f6
nathanielcx <nathanielc@uvic.ca>
2023-02-16 07:38:33 -0800

    Added argument to main.tf, fmt

--- keycloak-test/realms/moh_applications/main.tf
+++ keycloak-test/realms/moh_applications/main.tf
@@ -133,7 +133,8 @@ module "PLR-PRP" {
   source = "./plr-prp"
 }
 module "PLR-SHOPPERS" {
-  source = "./plr-shoppers"
+  source  = "./plr-shoppers"
+  PLR_IAT = module.PLR_IAT
 }
 module "PLR-MOH" {
   source = "./plr-moh"
