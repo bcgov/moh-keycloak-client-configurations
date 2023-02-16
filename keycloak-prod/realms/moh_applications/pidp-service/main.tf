6ffc015e21df928bfe911db96148b1c073058e3d
nathanielcx <nathanielc@uvic.ca>
2023-02-16 07:30:44 -0800

    removing prod role

--- keycloak-prod/realms/moh_applications/pidp-service/main.tf
+++ keycloak-prod/realms/moh_applications/pidp-service/main.tf
@@ -113,10 +113,6 @@ module "service-account-roles" {
       "client_id" = var.USER-MANAGEMENT-SERVICE.CLIENT.id,
       "role_id"   = "view-client-hcimweb"
     }
-    "USER-MANAGEMENT-SERVICE/view-client-licence-status" = {
-      "client_id" = var.USER-MANAGEMENT-SERVICE.CLIENT.id,
-      "role_id"   = "view-client-licence-status"
-    }
     "USER-MANAGEMENT-SERVICE/view-client-pidp-service" = {
       "client_id" = var.USER-MANAGEMENT-SERVICE.CLIENT.id,
       "role_id"   = "view-client-pidp-service"
