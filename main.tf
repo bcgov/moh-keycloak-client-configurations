67ab9cd5807a9fe153426db62c924a6843c0dbe8
Florek <filip.florek@cgi.com>
2023-03-02 11:36:36 -0700

    bump-mrparkers-version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     keycloak = {
       source  = "mrparkers/keycloak"
-      version = "= 4.1.0"
+      version = ">= 4.2.0"
     }
   }
   required_version = "~> 1.3.7"
