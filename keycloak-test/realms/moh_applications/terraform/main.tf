4dc8bee4004235c38104d0326b99a029e51be4db
Florek <filip.florek@cgi.com>
2023-03-03 15:06:04 -0700

    add terraform clients to test and prod

--- keycloak-test/realms/moh_applications/terraform/main.tf
+++ keycloak-test/realms/moh_applications/terraform/main.tf
@@ -23,36 +23,6 @@ resource "keycloak_openid_client" "CLIENT" {
   ]
 }
 
-resource "keycloak_openid_user_session_note_protocol_mapper" "Client-ID" {
-  add_to_id_token  = true
-  claim_name       = "clientId"
-  claim_value_type = "String"
-  client_id        = keycloak_openid_client.CLIENT.id
-  name             = "Client ID"
-  realm_id         = keycloak_openid_client.CLIENT.realm_id
-  session_note     = "clientId"
-}
-
-resource "keycloak_openid_user_session_note_protocol_mapper" "Client-Host" {
-  add_to_id_token  = true
-  claim_name       = "clientHost"
-  claim_value_type = "String"
-  client_id        = keycloak_openid_client.CLIENT.id
-  name             = "Client Host"
-  realm_id         = keycloak_openid_client.CLIENT.realm_id
-  session_note     = "clientHost"
-}
-
-resource "keycloak_openid_user_session_note_protocol_mapper" "Client-IP-Address" {
-  add_to_id_token  = true
-  claim_name       = "clientAddress"
-  claim_value_type = "String"
-  client_id        = keycloak_openid_client.CLIENT.id
-  name             = "Client IP Address"
-  realm_id         = keycloak_openid_client.CLIENT.realm_id
-  session_note     = "clientAddress"
-}
-
 module "scope-mappings" {
   source    = "../../../../modules/scope-mappings"
   realm_id  = keycloak_openid_client.CLIENT.realm_id
