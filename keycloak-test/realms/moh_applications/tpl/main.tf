08e4d489461d555b81fce5997757fbc22946be4f
AnumehaSrivastava05 <anumeha.saran@gmail.com>
2023-05-09 17:15:48 -0600

    Updated client description

--- keycloak-test/realms/moh_applications/tpl/main.tf
+++ keycloak-test/realms/moh_applications/tpl/main.tf
@@ -6,7 +6,7 @@ resource "keycloak_openid_client" "CLIENT" {
   client_authenticator_type           = "client-secret"
   client_id                           = "TPL"
   consent_required                    = false
-  description                         = "TPL Keycloak for SSO"
+  description                         = "Third Party Liability Keycloak for SSO"
   direct_access_grants_enabled        = false
   enabled                             = true
   frontchannel_logout_enabled         = false
