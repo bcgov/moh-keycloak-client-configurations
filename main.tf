283d464685c7dce27d30e2ce80a1c08b246dc7b8
Florek <filip.florek@cgi.com>
2023-05-24 10:13:29 -0600

    switch remote state update readme and add test change

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   required_version = "~> 1.3.7"
 
   backend "s3" {
-    bucket = "cey5wq-sandbox-moh-keycloak-terraform-state"
+    bucket = "cey5wq-tools-moh-keycloak-terraform-state"
     key    = "moh-keycloak-terraform-state"
     region = "ca-central-1"
   }
