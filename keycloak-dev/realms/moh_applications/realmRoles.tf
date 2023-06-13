f1a56247063b7aa2a87fc82a4c87ee60601fcb35
Florek <filip.florek@cgi.com>
2023-06-13 16:52:20 -0600

    clean up formatting errors

--- keycloak-dev/realms/moh_applications/realmRoles.tf
+++ keycloak-dev/realms/moh_applications/realmRoles.tf
@@ -1,11 +1,11 @@
 module "CGI-APPLICATION-SUPPORT" {
-  source                  = "./realm-roles/cgi-application-support"
-  BCER-CP                 = module.BCER-CP
-  HEM                     = module.HEM
-  HCIMWEB_HD2             = module.HCIMWEB_HD2
-  MSPDIRECT-SERVICE       = module.MSPDIRECT-SERVICE
-  HCIMWEB                 = module.HCIMWEB
-  PLR                     = module.PLR
+  source            = "./realm-roles/cgi-application-support"
+  BCER-CP           = module.BCER-CP
+  HEM               = module.HEM
+  HCIMWEB_HD2       = module.HCIMWEB_HD2
+  MSPDIRECT-SERVICE = module.MSPDIRECT-SERVICE
+  HCIMWEB           = module.HCIMWEB
+  PLR               = module.PLR
 }
 module "EXTERNAL-READ-ONLY" {
   source           = "./realm-roles/external-read-only"
