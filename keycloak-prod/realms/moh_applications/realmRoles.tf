81be175cd6cf5fed7a051ef37b8495760e193f89
Florek <filip.florek@cgi.com>
2023-06-15 09:53:48 -0600

    manage realm roles on prod env

---
+++ keycloak-prod/realms/moh_applications/realmRoles.tf
@@ -0,0 +1,24 @@
+module "CGI-APPLICATION-SUPPORT" {
+  source            = "./realm-roles/cgi-application-support"
+  BCER-CP           = module.BCER-CP
+  EACL              = module.EACL
+  FMDB              = module.FMDB
+  HEM               = module.HEM
+  MIWT              = module.MIWT
+  MSPDIRECT-SERVICE = module.MSPDIRECT-SERVICE
+  SFDS              = module.SFDS
+  SWT               = module.SWT
+}
+module "MANAGE-REALM" {
+  source           = "./realm-roles/manage-realm"
+  realm-management = module.realm-management
+}
+module "MANAGE-USERS" {
+  source                  = "./realm-roles/manage-users"
+  USER-MANAGEMENT         = module.USER-MANAGEMENT
+  USER-MANAGEMENT-SERVICE = module.USER-MANAGEMENT-SERVICE
+}
+module "VIEW-USERS" {
+  source                  = "./realm-roles/view-users"
+  USER-MANAGEMENT-SERVICE = module.USER-MANAGEMENT-SERVICE
+}
