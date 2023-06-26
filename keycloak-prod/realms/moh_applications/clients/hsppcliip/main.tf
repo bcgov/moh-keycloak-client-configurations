9d41b5bba7e878bdb3566b15cec273e969e9defd
dave.p.barrett <dave.p.barrett@cgi.com>
2023-06-26 13:21:57 -0700

    HSPPCLIIP - added prod configuration

---
+++ keycloak-prod/realms/moh_applications/clients/hsppcliip/main.tf
@@ -0,0 +1,58 @@
+resource "keycloak_openid_client" "CLIENT" {
+  access_token_lifespan               = ""
+  access_type                         = "CONFIDENTIAL"
+  backchannel_logout_session_required = true
+  base_url                            = ""
+  client_authenticator_type           = "client-secret"
+  client_id                           = "HSPPCLIIP"
+  consent_required                    = false
+  description                         = ""
+  direct_access_grants_enabled        = false
+  enabled                             = true
+  frontchannel_logout_enabled         = false
+  full_scope_allowed                  = false
+  implicit_flow_enabled               = false
+  name                                = "HSPPCLIIP"
+  pkce_code_challenge_method          = ""
+  realm_id                            = "moh_applications"
+  service_accounts_enabled            = false
+  standard_flow_enabled               = true
+  use_refresh_tokens                  = true
+  valid_redirect_uris = [
+    "https://hsppcliip.healthideas.gov.bc.ca/*",
+  ]
+  web_origins = [
+  ]
+}
+
+resource "keycloak_openid_user_client_role_protocol_mapper" "client_role_mapper" {
+  add_to_access_token         = true
+  add_to_id_token             = true
+  claim_name                  = "roles"
+  claim_value_type            = "String"
+  client_id                   = keycloak_openid_client.CLIENT.id
+  client_id_for_role_mappings = "HSPPCLIIP"
+  multivalued                 = true
+  name                        = "client roles"
+  realm_id                    = keycloak_openid_client.CLIENT.realm_id
+}
+
+module "client-roles" {
+  source    = "../../../../../modules/client-roles"
+  client_id = keycloak_openid_client.CLIENT.id
+  realm_id  = keycloak_openid_client.CLIENT.realm_id
+  roles = {
+    "HSPP_ALL" = {
+      "name" = "HSPP_ALL"
+    },
+    "HSPP_OKR" = {
+      "name" = "HSPP_OKR"
+    },
+    "HSPP_HumanResource" = {
+      "name" = "HSPP_HumanResource"
+    },
+    "HI_Administrator" = {
+      "name" = "HI_Administrator"
+    },
+  }
+}
