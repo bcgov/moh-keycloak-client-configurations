2d9c27a95ef931e8407ee99274a32c738a744057
dave.p.barrett <dave.p.barrett@cgi.com>
2023-07-11 15:19:17 -0700

    HSPP update redirect urls in test env

--- keycloak-test/realms/moh_applications/clients/hspp/main.tf
+++ keycloak-test/realms/moh_applications/clients/hspp/main.tf
@@ -19,7 +19,8 @@ resource "keycloak_openid_client" "CLIENT" {
   standard_flow_enabled               = true
   use_refresh_tokens                  = true
   valid_redirect_uris = [
-    "https://uathsppcliip.healthideas.gov.bc.ca/*",
+    "https://hsppstg.hlth.gov.bc.ca/*",
+    "https://localhost:44322/*"
   ]
   web_origins = [
   ]
