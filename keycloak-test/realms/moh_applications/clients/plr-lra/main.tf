f16f8d5fe71a6d997c3c9f51d7839a79fd303e78
David Sharpe <david.a.sharpe@cgi.com>
2023-07-28 13:51:13 -0700

    Removed unneeded mappers.

--- keycloak-test/realms/moh_applications/clients/plr-lra/main.tf
+++ keycloak-test/realms/moh_applications/clients/plr-lra/main.tf
@@ -33,33 +33,6 @@ resource "keycloak_openid_hardcoded_claim_protocol_mapper" "orgId" {
   name                = "orgId"
   realm_id            = keycloak_openid_client.CLIENT.realm_id
 }
-resource "keycloak_openid_user_session_note_protocol_mapper" "Client-Host" {
-  add_to_id_token  = true
-  claim_name       = "clientHost"
-  claim_value_type = "String"
-  client_id        = keycloak_openid_client.CLIENT.id
-  name             = "Client Host"
-  realm_id         = keycloak_openid_client.CLIENT.realm_id
-  session_note     = "clientHost"
-}
-resource "keycloak_openid_user_session_note_protocol_mapper" "Client-ID" {
-  add_to_id_token  = true
-  claim_name       = "clientId"
-  claim_value_type = "String"
-  client_id        = keycloak_openid_client.CLIENT.id
-  name             = "Client ID"
-  realm_id         = keycloak_openid_client.CLIENT.realm_id
-  session_note     = "clientId"
-}
-resource "keycloak_openid_user_session_note_protocol_mapper" "Client-IP-Address" {
-  add_to_id_token  = true
-  claim_name       = "clientAddress"
-  claim_value_type = "String"
-  client_id        = keycloak_openid_client.CLIENT.id
-  name             = "Client IP Address"
-  realm_id         = keycloak_openid_client.CLIENT.realm_id
-  session_note     = "clientAddress"
-}
 module "service-account-roles" {
   source                  = "../../../../../modules/service-account-roles"
   realm_id                = keycloak_openid_client.CLIENT.realm_id
