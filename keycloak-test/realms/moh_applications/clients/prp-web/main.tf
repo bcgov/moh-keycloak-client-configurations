dae49173cf478dfaf839507f43a1d098946565af
filipflorek <filip.florek@cgi.com>
2023-09-28 13:20:13 -0600

    add confidential client specfic settings

--- keycloak-test/realms/moh_applications/clients/prp-web/main.tf
+++ keycloak-test/realms/moh_applications/clients/prp-web/main.tf
@@ -1,14 +1,23 @@
 resource "keycloak_openid_client" "CLIENT" {
-  access_token_lifespan      = ""
-  access_type                = "CONFIDENTIAL"
-  client_authenticator_type  = "client-secret"
-  client_id                  = "PRP-WEB"
-  description                = "Provider Reporting Portal Frontend"
-  full_scope_allowed         = false
-  name                       = "PRP Web"
-  pkce_code_challenge_method = "S256"
-  realm_id                   = "moh_applications"
-  standard_flow_enabled      = true
+  access_token_lifespan               = ""
+  access_type                         = "CONFIDENTIAL"
+  backchannel_logout_session_required = true
+  base_url                            = ""
+  client_authenticator_type           = "client-secret"
+  client_id                           = "PRP-WEB"
+  consent_required                    = false
+  description                         = "Provider Reporting Portal Frontend"
+  direct_access_grants_enabled        = false
+  enabled                             = true
+  frontchannel_logout_enabled         = false
+  full_scope_allowed                  = false
+  implicit_flow_enabled               = false
+  name                                = "PRP Web"
+  pkce_code_challenge_method          = "S256"
+  realm_id                            = "moh_applications"
+  service_accounts_enabled            = false
+  standard_flow_enabled               = true
+  use_refresh_tokens                  = true
   valid_redirect_uris = [
     "http://localhost:*",
     "https://wonderful-cliff-0d1cec610.2.azurestaticapps.net/*",
