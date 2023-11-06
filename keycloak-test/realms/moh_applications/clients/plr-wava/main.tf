98dbe805413a8f56487fc59a9ba15f8c362b8fd4
dave.p.barrett <dave.p.barrett@cgi.com>
2023-11-06 15:15:37 -0800

    Add test client for PLR WAVA scan

--- keycloak-test/realms/moh_applications/clients/plr-wava/main.tf
+++ keycloak-test/realms/moh_applications/clients/plr-wava/main.tf
@@ -23,14 +23,43 @@ resource "keycloak_openid_client" "CLIENT" {
   web_origins = [
   ]
 }
+resource "keycloak_openid_hardcoded_claim_protocol_mapper" "orgId" {
+  add_to_access_token = true
+  add_to_id_token     = true
+  add_to_userinfo     = true
+  claim_name          = "orgId"
+  claim_value         = "00002855"
+  claim_value_type    = "String"
+  client_id           = keycloak_openid_client.CLIENT.id
+  name                = "orgId"
+  realm_id            = keycloak_openid_client.CLIENT.realm_id
+}
 
-module "client-roles" {
-  source    = "../../../../../modules/client-roles"
-  client_id = keycloak_openid_client.CLIENT.id
+module "service-account-roles" {
+  source                  = "../../../../../modules/service-account-roles"
+  realm_id                = keycloak_openid_client.CLIENT.realm_id
+  client_id               = keycloak_openid_client.CLIENT.id
+  service_account_user_id = keycloak_openid_client.CLIENT.service_account_user_id
+  realm_roles = {
+    "default-roles-moh_applications" = "default-roles-moh_applications",
+  }
+  client_roles = {
+    "PLR_REV/PRIMARY_SOURCE" = {
+      "client_id" = var.PLR_REV.CLIENT.id,
+      "role_id"   = "PRIMARY_SOURCE"
+    }
+    "PLR_REV/CONSUMER" = {
+      "client_id" = var.PLR_REV.CLIENT.id,
+      "role_id"   = "CONSUMER"
+    }
+  }
+}
+module "scope-mappings" {
+  source    = "../../../../../modules/scope-mappings"
   realm_id  = keycloak_openid_client.CLIENT.realm_id
+  client_id = keycloak_openid_client.CLIENT.id
   roles = {
-    "CONSUMER" = {
-      "name" = "CONSUMER"
-    },
+    "PLR_REV/CONSUMER"  = var.PLR_REV.ROLES["PRIMARY_SOURCE"].id
+    "PLR_REV/CONSUMER"  = var.PLR_REV.ROLES["CONSUMER"].id
   }
 }
