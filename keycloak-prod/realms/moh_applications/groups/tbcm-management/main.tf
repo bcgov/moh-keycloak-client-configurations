d5b8cc3270cb0d486e57e45d2a75c81be1be890b
filipflorek <52381251+filipflorek@users.noreply.github.com>
2023-11-16 15:22:28 -0700

    create-tbcm-management-prod-group (#382)
    * create-tbcm-management-prod-group
    
    * enable tbcm in umc prod

---
+++ keycloak-prod/realms/moh_applications/groups/tbcm-management/main.tf
@@ -0,0 +1,19 @@
+resource "keycloak_group" "GROUP" {
+  realm_id = "moh_applications"
+  name     = "TBCM Management"
+}
+
+resource "keycloak_group_roles" "GROUP_ROLES" {
+  realm_id = keycloak_group.GROUP.realm_id
+  group_id = keycloak_group.GROUP.id
+
+  role_ids = [
+    var.USER-MANAGEMENT-SERVICE.ROLES["manage-user-roles"].id,
+    var.USER-MANAGEMENT-SERVICE.ROLES["view-client-tbcm"].id,
+    var.USER-MANAGEMENT-SERVICE.ROLES["view-clients"].id,
+    var.USER-MANAGEMENT-SERVICE.ROLES["view-events"].id,
+    var.USER-MANAGEMENT-SERVICE.ROLES["view-groups"].id,
+    var.USER-MANAGEMENT-SERVICE.ROLES["view-metrics"].id,
+    var.USER-MANAGEMENT-SERVICE.ROLES["view-users"].id
+  ]
+}
