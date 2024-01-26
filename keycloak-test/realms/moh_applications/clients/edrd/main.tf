0b49fff036cfd5912755d700a8c760193a4eb954
filipflorek <filip.florek@cgi.com>
2024-01-26 10:53:49 -0700

    update edrd redirect urls

--- keycloak-test/realms/moh_applications/clients/edrd/main.tf
+++ keycloak-test/realms/moh_applications/clients/edrd/main.tf
@@ -21,6 +21,10 @@ resource "keycloak_openid_client" "CLIENT" {
   valid_redirect_uris = [
     "https://bchealth--edrduat.sandbox.my.salesforce.com/*",
     "https://bchealth--edrduat.sandbox.my.site.com/*",
+    "https://bchealth--edrddev.sandbox.my.site.com/edrdportal/*",
+    "bchealth--edrddev.sandbox.my.salesforce.com/*",
+    "bchealth--edrdqa.sandbox.my.salesforce.com/*",
+    "https://bchealth--edrdqa.sandbox.my.site.com/edrdportalQA/*",
   ]
   web_origins = [
   ]
