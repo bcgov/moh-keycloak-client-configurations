ce3f734b7103b36a66995b5a77f2259082b2904a
filipflorek <52381251+filipflorek@users.noreply.github.com>
2024-03-14 10:38:39 -0600

    add ALR redirects (#477)

--- keycloak-test/realms/moh_applications/clients/alr/main.tf
+++ keycloak-test/realms/moh_applications/clients/alr/main.tf
@@ -28,6 +28,9 @@ resource "keycloak_openid_client" "CLIENT" {
     "https://bcministryofhealth-environmentalh2--hotfixqa.sandbox.my.salesforce.com/*",
     "https://bcministryofhealth-environmentalh2--hotfixdev.sandbox.my.salesforce.com/*",
     "https://bcministryofhealth-environmentalh2--hotfixqa.sandbox.lightning.force.com/*",
+    "https://bcministryofhealth-environmentalh2--qa.sandbox.my.salesforce.com/*",
+    "https://bcministryofhealth-environmentalh2--demo.sandbox.my.salesforce.com/*",
+    "https://bcministryofhealth-environmentalh2--uat.sandbox.my.salesforce.com/*"
   ]
   web_origins = [
   ]
