72f98e64cfd68cee77e08c0ff8385bdc33cb92b6
David Sharpe <david.a.sharpe@cgi.com>
2024-04-08 16:30:25 -0700

    Enable event tracking for Keycloak DEV moh_applications realm with annual expiration.

---
+++ keycloak-dev/realms/moh_applications/events.tf
@@ -0,0 +1,18 @@
+resource "keycloak_realm_events" "realm_events" {
+  realm_id = "moh_applications"
+
+  events_enabled    = true
+  # There are 31,536,000 seconds in a year.
+  events_expiration = 31536000
+
+  admin_events_enabled         = true
+  admin_events_details_enabled = true
+
+  # When omitted or left empty, keycloak will enable all event types
+  enabled_event_types = [
+  ]
+
+  events_listeners = [
+    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
+  ]
+}
