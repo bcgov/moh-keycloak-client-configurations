095647482632e059eaf30ae00019550472228e82
David Sharpe <david.a.sharpe@cgi.com>
2024-04-16 15:27:38 -0700

    Document and organize.

--- keycloak-dev/events.tf
+++ keycloak-dev/events.tf
@@ -1,7 +1,11 @@
-resource "keycloak_realm_events" "realm_events_idir_aad" {
-  realm_id = "idir_aad"
+######################
+# APPLICATION REALMS #
+######################
 
-  events_enabled = false
+resource "keycloak_realm_events" "realm_events_bcerd" {
+  realm_id = "bcerd"
+
+  events_enabled = true
   # There are 31,536,000 seconds in a year.
   events_expiration = 31536000
 
@@ -16,6 +20,143 @@ resource "keycloak_realm_events" "realm_events_idir_aad" {
     "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
   ]
 }
+
+resource "keycloak_realm_events" "realm_events_lra" {
+  realm_id = "lra"
+
+  events_enabled = true
+  # There are 31,536,000 seconds in a year.
+  events_expiration = 31536000
+
+  admin_events_enabled         = true
+  admin_events_details_enabled = true
+
+  # When omitted or left empty, keycloak will enable all event types
+  enabled_event_types = [
+  ]
+
+  events_listeners = [
+    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
+  ]
+}
+
+resource "keycloak_realm_events" "realm_events_master" {
+  realm_id = "master"
+
+  events_enabled = true
+  # There are 31,536,000 seconds in a year.
+  events_expiration = 31536000
+
+  admin_events_enabled         = true
+  admin_events_details_enabled = true
+
+  # When omitted or left empty, keycloak will enable all event types
+  enabled_event_types = [
+  ]
+
+  events_listeners = [
+    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
+  ]
+}
+
+resource "keycloak_realm_events" "realm_events_mhsu_foundry" {
+  realm_id = "mhsu_foundry"
+
+  events_enabled = true
+  # There are 31,536,000 seconds in a year.
+  events_expiration = 31536000
+
+  admin_events_enabled         = true
+  admin_events_details_enabled = true
+
+  # When omitted or left empty, keycloak will enable all event types
+  enabled_event_types = [
+  ]
+
+  events_listeners = [
+    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
+  ]
+}
+
+resource "keycloak_realm_events" "realm_events_moh_applications" {
+  realm_id = "moh_applications"
+
+  events_enabled = true
+  # There are 31,536,000 seconds in a year.
+  events_expiration = 31536000
+
+  admin_events_enabled         = true
+  admin_events_details_enabled = true
+
+  # When omitted or left empty, keycloak will enable all event types
+  enabled_event_types = [
+  ]
+
+  events_listeners = [
+    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
+  ]
+}
+
+resource "keycloak_realm_events" "realm_events_moh_citizen" {
+  realm_id = "moh_citizen"
+
+  events_enabled = true
+  # There are 31,536,000 seconds in a year.
+  events_expiration = 31536000
+
+  admin_events_enabled         = true
+  admin_events_details_enabled = true
+
+  # When omitted or left empty, keycloak will enable all event types
+  enabled_event_types = [
+  ]
+
+  events_listeners = [
+    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
+  ]
+}
+
+resource "keycloak_realm_events" "realm_events_pidp_sandbox" {
+  realm_id = "pidp_sandbox"
+
+  events_enabled = true
+  # There are 31,536,000 seconds in a year.
+  events_expiration = 31536000
+
+  admin_events_enabled         = true
+  admin_events_details_enabled = true
+
+  # When omitted or left empty, keycloak will enable all event types
+  enabled_event_types = [
+  ]
+
+  events_listeners = [
+    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
+  ]
+}
+
+resource "keycloak_realm_events" "realm_events_v2_pos" {
+  realm_id = "v2_pos"
+
+  events_enabled = true
+  # There are 31,536,000 seconds in a year.
+  events_expiration = 31536000
+
+  admin_events_enabled         = true
+  admin_events_details_enabled = true
+
+  # When omitted or left empty, keycloak will enable all event types
+  enabled_event_types = [
+  ]
+
+  events_listeners = [
+    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
+  ]
+}
+
+##############
+# IDP REALMS #
+##############
 
 resource "keycloak_realm_events" "realm_events_bceid_basic" {
   realm_id = "bceid_basic"
@@ -40,25 +181,6 @@ resource "keycloak_realm_events" "realm_events_bceid_business" {
   realm_id = "bceid_business"
 
   events_enabled = false
-  # There are 31,536,000 seconds in a year.
-  events_expiration = 31536000
-
-  admin_events_enabled         = true
-  admin_events_details_enabled = true
-
-  # When omitted or left empty, keycloak will enable all event types
-  enabled_event_types = [
-  ]
-
-  events_listeners = [
-    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
-  ]
-}
-
-resource "keycloak_realm_events" "realm_events_bcerd" {
-  realm_id = "bcerd"
-
-  events_enabled = true
   # There are 31,536,000 seconds in a year.
   events_expiration = 31536000
 
@@ -131,25 +253,6 @@ resource "keycloak_realm_events" "realm_events_bcsc" {
   ]
 }
 
-resource "keycloak_realm_events" "realm_events_moh_citizen" {
-  realm_id = "moh_citizen"
-
-  events_enabled = true
-  # There are 31,536,000 seconds in a year.
-  events_expiration = 31536000
-
-  admin_events_enabled         = true
-  admin_events_details_enabled = true
-
-  # When omitted or left empty, keycloak will enable all event types
-  enabled_event_types = [
-  ]
-
-  events_listeners = [
-    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
-  ]
-}
-
 resource "keycloak_realm_events" "realm_events_fnha_aad" {
   realm_id = "fnha_aad"
 
@@ -188,29 +291,10 @@ resource "keycloak_realm_events" "realm_events_idir" {
   ]
 }
 
-resource "keycloak_realm_events" "realm_events_lra" {
-  realm_id = "lra"
+resource "keycloak_realm_events" "realm_events_idir_aad" {
+  realm_id = "idir_aad"
 
-  events_enabled = true
-  # There are 31,536,000 seconds in a year.
-  events_expiration = 31536000
-
-  admin_events_enabled         = true
-  admin_events_details_enabled = true
-
-  # When omitted or left empty, keycloak will enable all event types
-  enabled_event_types = [
-  ]
-
-  events_listeners = [
-    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
-  ]
-}
-
-resource "keycloak_realm_events" "realm_events_master" {
-  realm_id = "master"
-
-  events_enabled = true
+  events_enabled = false
   # There are 31,536,000 seconds in a year.
   events_expiration = 31536000
 
@@ -230,44 +314,6 @@ resource "keycloak_realm_events" "realm_events_mhsu_ehs" {
   realm_id = "mhsu_ehs"
 
   events_enabled = false
-  # There are 31,536,000 seconds in a year.
-  events_expiration = 31536000
-
-  admin_events_enabled         = true
-  admin_events_details_enabled = true
-
-  # When omitted or left empty, keycloak will enable all event types
-  enabled_event_types = [
-  ]
-
-  events_listeners = [
-    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
-  ]
-}
-
-resource "keycloak_realm_events" "realm_events_mhsu_foundry" {
-  realm_id = "mhsu_foundry"
-
-  events_enabled = true
-  # There are 31,536,000 seconds in a year.
-  events_expiration = 31536000
-
-  admin_events_enabled         = true
-  admin_events_details_enabled = true
-
-  # When omitted or left empty, keycloak will enable all event types
-  enabled_event_types = [
-  ]
-
-  events_listeners = [
-    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
-  ]
-}
-
-resource "keycloak_realm_events" "realm_events_moh_applications" {
-  realm_id = "moh_applications"
-
-  events_enabled = true
   # There are 31,536,000 seconds in a year.
   events_expiration = 31536000
 
@@ -340,40 +386,3 @@ resource "keycloak_realm_events" "realm_events_phsa_aad" {
   ]
 }
 
-resource "keycloak_realm_events" "realm_events_pidp_sandbox" {
-  realm_id = "pidp_sandbox"
-
-  events_enabled = true
-  # There are 31,536,000 seconds in a year.
-  events_expiration = 31536000
-
-  admin_events_enabled         = true
-  admin_events_details_enabled = true
-
-  # When omitted or left empty, keycloak will enable all event types
-  enabled_event_types = [
-  ]
-
-  events_listeners = [
-    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
-  ]
-}
-
-resource "keycloak_realm_events" "realm_events_v2_pos" {
-  realm_id = "v2_pos"
-
-  events_enabled = true
-  # There are 31,536,000 seconds in a year.
-  events_expiration = 31536000
-
-  admin_events_enabled         = true
-  admin_events_details_enabled = true
-
-  # When omitted or left empty, keycloak will enable all event types
-  enabled_event_types = [
-  ]
-
-  events_listeners = [
-    "jboss-logging", # keycloak enables the 'jboss-logging' event listener by default.
-  ]
-}
