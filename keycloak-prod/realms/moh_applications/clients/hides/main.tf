c3bb7b7ad8b7551b0a64d398433866caf17a68fb
filipflorek <filip.florek@cgi.com>
2024-06-03 13:54:53 -0600

    fix format

--- keycloak-prod/realms/moh_applications/clients/hides/main.tf
+++ keycloak-prod/realms/moh_applications/clients/hides/main.tf
@@ -19,7 +19,7 @@ resource "keycloak_openid_client" "CLIENT" {
   standard_flow_enabled               = true
   use_refresh_tokens                  = true
   valid_redirect_uris = [
-  "https://secure.healthideas.gov.bc.ca/hides/*"
+    "https://secure.healthideas.gov.bc.ca/hides/*"
   ]
   web_origins = [
     "+"
