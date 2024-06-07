979b72019a908c18b7b2ed4148146bcfa07cc5b6
David Sharpe <david.a.sharpe@cgi.com>
2024-06-07 14:15:05 -0700

    Fix formatting.

--- keycloak-dev/realms/moh_applications/clients/forms/main.tf
+++ keycloak-dev/realms/moh_applications/clients/forms/main.tf
@@ -73,7 +73,7 @@ resource "keycloak_openid_client_optional_scopes" "client_optional_scopes" {
 resource "keycloak_generic_client_protocol_mapper" "phsa_windowsaccountname" {
   realm_id        = keycloak_openid_client.CLIENT.realm_id
   client_id       = keycloak_openid_client.CLIENT.id
-   name            = "preferred_username"
+  name            = "preferred_username"
   protocol        = "openid-connect"
   protocol_mapper = "oidc-override-usermodel-attribute-mapper"
   config = {
