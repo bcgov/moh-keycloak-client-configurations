85ffdbf1fe6764db86e1cad31f6ea5378f769df4
David Sharpe <david.a.sharpe@cgi.com>
2024-06-27 15:44:23 -0700

    Correct formatting.

--- keycloak-dev/events.tf
+++ keycloak-dev/events.tf
@@ -18,7 +18,7 @@ locals {
     "VALIDATE_ACCESS_TOKEN",
     "VALIDATE_ACCESS_TOKEN_ERROR"
   ]
-  seconds_in_a_year = 31536000
+  seconds_in_a_year      = 31536000
   seconds_in_three_years = seconds_in_a_year * 3
 }
 
