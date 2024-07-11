8958eacd6fffabf8aa553fdeca0b39e2fc070c11
filipflorek <filip.florek@cgi.com>
2024-07-11 09:11:24 -0600

    update edrd-portal umc permissions

--- keycloak-test/realms/moh_applications/realm-roles/manage-users/main.tf
+++ keycloak-test/realms/moh_applications/realm-roles/manage-users/main.tf
@@ -12,6 +12,7 @@ resource "keycloak_role" "REALM_ROLE" {
     var.USER-MANAGEMENT-SERVICE.ROLES["view-client-eacl"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["view-client-eacl_stg"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["view-client-edrd"].id,
+    var.USER-MANAGEMENT-SERVICE.ROLES["view-client-edrd-portal"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["view-client-emcod"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["view-client-fmdb"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["view-client-gis"].id,
