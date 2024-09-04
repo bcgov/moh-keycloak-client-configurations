69539310af86a838575140844e78733f70e522be
David Sharpe <david.a.sharpe@cgi.com>
2024-09-04 14:49:30 -0700

    EMACCS DEV was created in the wrong realm. Move to MOH_CITIZEN.

--- keycloak-dev/realms/moh_citizen/clients/emaccs/main.tf
+++ keycloak-dev/realms/moh_citizen/clients/emaccs/main.tf
@@ -1,19 +1,27 @@
-module "payara-client" {
-  source                             = "../../../../../modules/payara-client"
-  base_url                           = ""
-  claim_name                         = "EMACCS_role_not_used"
-  client_id                          = "EMACCS"
-  client_name                        = "EMACCS"
-  client_role_mapper_add_to_id_token = false
-  client_role_mapper_add_to_userinfo = false
-  description                        = "Emergency Medical Assistant Continuing Competency System"
-  mapper_name                        = "EMACCS Role - Not Used"
-  roles = {
-  }
-  service_accounts_enabled = true
+resource "keycloak_openid_client" "CLIENT" {
+  access_token_lifespan               = ""
+  access_type                         = "CONFIDENTIAL"
+  backchannel_logout_session_required = true
+  base_url                            = ""
+  client_authenticator_type           = "client-secret"
+  client_id                           = "EMACCS"
+  consent_required                    = false
+  description                         = "Emergency Medical Assistant Continuing Competency System"
+  direct_access_grants_enabled        = false
+  enabled                             = true
+  frontchannel_logout_enabled         = false
+  full_scope_allowed                  = false
+  implicit_flow_enabled               = false
+  name                                = "EMACCS"
+  pkce_code_challenge_method          = ""
+  realm_id                            = "moh_citizen"
+  service_accounts_enabled            = false
+  standard_flow_enabled               = true
   valid_redirect_uris = [
     "http://localhost:8080/*",
     "https://emaccsd.hlth.gov.bc.ca/*",
     "https://logontest7.gov.bc.ca/clp-cgi/logoff.cgi*",
+  ]
+  web_origins = [
   ]
 }
