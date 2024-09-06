9ed009a670f2c8ddc185d1297e39cfcc0b6a80c8
filipflorek <filip.florek@cgi.com>
2024-09-06 15:56:17 -0600

    fix wording

--- keycloak-dev/realms/moh_applications/clientScopes.tf
+++ keycloak-dev/realms/moh_applications/clientScopes.tf
@@ -62,59 +62,59 @@ resource "keycloak_openid_client_scope" "moh_idp_openid_client_scope" {
 resource "keycloak_saml_client_scope" "idir_saml_client_scope" {
   realm_id    = "moh_applications"
   name        = "idir-saml"
-  description = "Assign this scope to an SAML client using IDP restriction module to allow logging in with IDIR."
+  description = "Assign this scope to a SAML client using IDP restriction module to allow logging in with IDIR."
 }
 
 resource "keycloak_saml_client_scope" "idir_aad_saml_client_scope" {
   realm_id    = "moh_applications"
   name        = "idir_aad-saml"
-  description = "Assign this scope to an SAML client using IDP restriction module to allow logging in with IDIR MFA."
+  description = "Assign this scope to a SAML client using IDP restriction module to allow logging in with IDIR MFA."
 }
 
 resource "keycloak_saml_client_scope" "phsa_saml_client_scope" {
   realm_id    = "moh_applications"
   name        = "phsa-saml"
-  description = "Assign this scope to an SAML client using IDP restriction module to allow logging in with Health Authority."
+  description = "Assign this scope to a SAML client using IDP restriction module to allow logging in with Health Authority."
 }
 
 resource "keycloak_saml_client_scope" "phsa_aad_saml_client_scope" {
   realm_id    = "moh_applications"
   name        = "phsa_aad-saml"
-  description = "Assign this scope to an SAML client using IDP restriction module to allow logging in with Health Authority MFA."
+  description = "Assign this scope to a SAML client using IDP restriction module to allow logging in with Health Authority MFA."
 }
 
 resource "keycloak_saml_client_scope" "fnha_aad_saml_client_scope" {
   realm_id    = "moh_applications"
   name        = "fnha_aad-saml"
-  description = "Assign this scope to an SAML client using IDP restriction module to allow logging in with FNHA MFA."
+  description = "Assign this scope to a SAML client using IDP restriction module to allow logging in with FNHA MFA."
 }
 
 resource "keycloak_saml_client_scope" "bceid_business_saml_client_scope" {
   realm_id    = "moh_applications"
   name        = "bceid_business-saml"
-  description = "Assign this scope to an SAML client using IDP restriction module to allow logging in with BceID Business."
+  description = "Assign this scope to a SAML client using IDP restriction module to allow logging in with BceID Business."
 }
 
 resource "keycloak_saml_client_scope" "bcprovider_aad_saml_client_scope" {
   realm_id    = "moh_applications"
   name        = "bcprovider_aad-saml"
-  description = "Assign this scope to an SAML client using IDP restriction module to allow logging in with BC Provider."
+  description = "Assign this scope to a SAML client using IDP restriction module to allow logging in with BC Provider."
 }
 
 resource "keycloak_saml_client_scope" "bcsc_saml_client_scope" {
   realm_id    = "moh_applications"
   name        = "bcsc-saml"
-  description = "Assign this scope to an SAML client using IDP restriction module to allow logging in with BC Services Card for PIdP."
+  description = "Assign this scope to a SAML client using IDP restriction module to allow logging in with BC Services Card for PIdP."
 }
 
 resource "keycloak_saml_client_scope" "bcsc_mspdirect_saml_client_scope" {
   realm_id    = "moh_applications"
   name        = "bcsc_mspdirect-saml"
-  description = "Assign this scope to an SAML client using IDP restriction module to allow logging in with BC Services Card for MSPDirect."
+  description = "Assign this scope to a SAML client using IDP restriction module to allow logging in with BC Services Card for MSPDirect."
 }
 
 resource "keycloak_saml_client_scope" "moh_idp_openid_client_scope" {
   realm_id    = "moh_applications"
   name        = "moh_idp-saml"
-  description = "Assign this scope to an SAML client using IDP restriction module to allow logging in with Keycloak."
+  description = "Assign this scope to a SAML client using IDP restriction module to allow logging in with Keycloak."
 }
