6bbb9426626de081a83cdf39c0e07e7cb29978ba
filipflorek <52381251+filipflorek@users.noreply.github.com>
2024-09-24 13:05:18 -0600

    add bulk-removal permissions (#614)

--- keycloak-test/realms/moh_applications/clients/user-management/main.tf
+++ keycloak-test/realms/moh_applications/clients/user-management/main.tf
@@ -52,6 +52,7 @@ module "scope-mappings" {
   realm_id  = keycloak_openid_client.CLIENT.realm_id
   client_id = keycloak_openid_client.CLIENT.id
   roles = {
+    "USER-MANAGEMENT-SERVICE/bulk-removal"                          = var.USER-MANAGEMENT-SERVICE.ROLES["bulk-removal"].id,
     "USER-MANAGEMENT-SERVICE/create-user"                           = var.USER-MANAGEMENT-SERVICE.ROLES["create-user"].id,
     "USER-MANAGEMENT-SERVICE/manage-user-details"                   = var.USER-MANAGEMENT-SERVICE.ROLES["manage-user-details"].id,
     "USER-MANAGEMENT-SERVICE/manage-all-groups"                     = var.USER-MANAGEMENT-SERVICE.ROLES["manage-all-groups"].id,
