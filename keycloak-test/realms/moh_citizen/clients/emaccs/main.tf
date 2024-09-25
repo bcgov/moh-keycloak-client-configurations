124e81fe2631b70e5b7ee06e378d98b4174baf88
SScholefield <sofia.scholefield@cgi.com>
2024-09-25 14:32:04 -0700

    fix duplicate

--- keycloak-test/realms/moh_citizen/clients/emaccs/main.tf
+++ keycloak-test/realms/moh_citizen/clients/emaccs/main.tf
@@ -28,7 +28,7 @@ resource "keycloak_openid_client" "CLIENT" {
 }
 resource "keycloak_openid_user_attribute_protocol_mapper" "idir_user_guid" {
   add_to_access_token = true
-  add_to_userinfo = var.client_role_mapper_add_to_id_token
+  add_to_id_token = var.client_role_mapper_add_to_id_token
   add_to_userinfo = var.client_role_mapper_add_to_userinfo
   claim_name      = "idir_user_guid"
   client_id       = keycloak_openid_client.CLIENT.id
@@ -38,7 +38,7 @@ resource "keycloak_openid_user_attribute_protocol_mapper" "idir_user_guid" {
 }
 resource "keycloak_openid_user_attribute_protocol_mapper" "bceid_user_guid" {
   add_to_access_token = true
-  add_to_userinfo = var.client_role_mapper_add_to_id_token
+  add_to_id_token = var.client_role_mapper_add_to_id_token
   add_to_userinfo = var.client_role_mapper_add_to_userinfo
   claim_name      = "bceid_user_guid"
   client_id       = keycloak_openid_client.CLIENT.id
