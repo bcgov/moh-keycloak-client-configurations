15cb484e70cb8ec467c343a242d71ac63ac690fc
filipflorek <filip.florek@cgi.com>
2024-09-25 10:18:08 -0600

    update dependencies

--- keycloak-prod/realms/moh_applications/clients.tf
+++ keycloak-prod/realms/moh_applications/clients.tf
@@ -223,6 +223,7 @@ module "PLR-REGADMIN" {
 }
 module "PNET-API-CLIENT-GENERATION" {
   source = "./clients/pnet-api-client-generation"
+  realm-management = module.realm-management
 }
 module "PPM-API-CGI-BC30550160" {
   source = "./clients/ppm-api-cgi-BC30550160"
