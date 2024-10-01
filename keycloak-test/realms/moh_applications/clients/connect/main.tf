d24c91e4d675c4962b79cefd707555000267b98f
filipflorek <filip.florek@cgi.com>
2024-10-01 09:53:04 -0600

    fix formatting errors

--- keycloak-test/realms/moh_applications/clients/connect/main.tf
+++ keycloak-test/realms/moh_applications/clients/connect/main.tf
@@ -28,7 +28,7 @@ resource "keycloak_openid_client" "CLIENT" {
   ]
   web_origins = [
   ]
-   authentication_flow_binding_overrides {
+  authentication_flow_binding_overrides {
     #browser-idp-restriction flow
     browser_id = var.browser_idp_restriction_flow
   }
