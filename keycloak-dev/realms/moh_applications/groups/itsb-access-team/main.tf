32d4d712c31a88cab0a92d2f30cc2f46714d78f9
filipflorek <filip.florek@cgi.com>
2024-10-18 13:25:45 -0600

    update itsb-access-team permissions

--- keycloak-dev/realms/moh_applications/groups/itsb-access-team/main.tf
+++ keycloak-dev/realms/moh_applications/groups/itsb-access-team/main.tf
@@ -14,6 +14,7 @@ resource "keycloak_group_roles" "GROUP_ROLES" {
     var.USER-MANAGEMENT-SERVICE.ROLES["manage-user-roles"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["view-client-bcer-cp"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["view-client-pho-rsc"].id,
+    var.USER-MANAGEMENT-SERVICE.ROLES["view-client-pho-rsc-groups"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["view-client-sa-dbaac-portal"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["view-client-sa-hibc-service-bc-portal"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["view-client-sa-sfdc"].id,
