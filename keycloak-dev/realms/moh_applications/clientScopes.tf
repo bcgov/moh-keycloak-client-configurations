9d0c31e83d2426eb15efaf13f64a483140c58f1d
David Sharpe <david.a.sharpe@cgi.com>
2025-01-08 19:49:51 -0800

    Create Yukon AAD scope in DEV for IDP restriction module use.

--- keycloak-dev/realms/moh_applications/clientScopes.tf
+++ keycloak-dev/realms/moh_applications/clientScopes.tf
@@ -52,11 +52,16 @@ resource "keycloak_openid_client_scope" "bcsc_mspdirect_openid_client_scope" {
   description = "Assign this scope to an OIDC client using IDP restriction module to allow logging in with BC Services Card for MSPDirect."
 }
 
-
 resource "keycloak_openid_client_scope" "moh_idp_openid_client_scope" {
   realm_id    = "moh_applications"
   name        = "moh_idp"
   description = "Assign this scope to an OIDC client using IDP restriction module to allow logging in with Keycloak."
+}
+
+resource "keycloak_openid_client_scope" "yukon_aad_openid_client_scope" {
+  realm_id    = "moh_applications"
+  name        = "yukon_aad"
+  description = "Assign this scope to an OIDC client using IDP restriction module to allow logging in with Yukon MFA."
 }
 
 resource "keycloak_saml_client_scope" "idir_saml_client_scope" {
@@ -117,4 +122,10 @@ resource "keycloak_saml_client_scope" "moh_idp_openid_client_scope" {
   realm_id    = "moh_applications"
   name        = "moh_idp-saml"
   description = "Assign this scope to a SAML client using IDP restriction module to allow logging in with Keycloak."
+}
+
+resource "keycloak_saml_client_scope" "yukon_aad_saml_client_scope" {
+  realm_id    = "moh_applications"
+  name        = "yukon_aad-saml"
+  description = "Assign this scope to a SAML client using IDP restriction module to allow logging in with Yukon MFA."
 }
