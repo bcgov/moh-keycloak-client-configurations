70e3885071f07ece9ffec384b526584c87673f9c
filipflorek <52381251+filipflorek@users.noreply.github.com>
2025-01-23 17:04:59 -0700

    update florizel admin permissions (#680)

--- keycloak-prod/realms/moh_applications/groups/florizel-management/main.tf
+++ keycloak-prod/realms/moh_applications/groups/florizel-management/main.tf
@@ -8,6 +8,7 @@ resource "keycloak_group_roles" "GROUP_ROLES" {
   group_id = keycloak_group.GROUP.id
 
   role_ids = [
+    var.USER-MANAGEMENT-SERVICE.ROLES["create-user"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["manage-own-groups"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["manage-user-roles"].id,
     var.USER-MANAGEMENT-SERVICE.ROLES["view-clients"].id,
