e0c7b43fe75e1f89fc08eec97a192e5b78c230d8
filipflorek <filip.florek@cgi.com>
2025-03-11 14:07:16 -0600

    code review notes

--- keycloak-dev/realms/moh_applications/clients/ien/main.tf
+++ keycloak-dev/realms/moh_applications/clients/ien/main.tf
@@ -21,7 +21,7 @@ resource "keycloak_openid_client" "CLIENT" {
   valid_redirect_uris = [
     "http://localhost:*",
     "https://localhost:*",
-    "https://dev.ien.gov.bc.ca/*"
+    "https://dev.ien.gov.bc.ca/*",
   ]
   web_origins = [
     "*",
--- keycloak-test/realms/moh_applications/clients/ien/main.tf
+++ keycloak-test/realms/moh_applications/clients/ien/main.tf
@@ -19,7 +19,7 @@ resource "keycloak_openid_client" "CLIENT" {
   standard_flow_enabled               = true
   use_refresh_tokens                  = true
   valid_redirect_uris = [
-    "https://test.ien.gov.bc.ca/*"
+    "https://test.ien.gov.bc.ca/*",
   ]
   web_origins = [
     "+",
