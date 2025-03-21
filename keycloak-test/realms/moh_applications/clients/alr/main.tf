8919faf41a38bed26ccf3813b4e99c4d0b76f6e9
filipflorek <52381251+filipflorek@users.noreply.github.com>
2025-03-21 15:39:21 -0600

    update alr config (#712)
    * update alr config
    
    * code review fix

--- keycloak-test/realms/moh_applications/clients/alr/main.tf
+++ keycloak-test/realms/moh_applications/clients/alr/main.tf
@@ -31,7 +31,9 @@ resource "keycloak_openid_client" "CLIENT" {
     "https://bcministryofhealth-environmentalh2--qa.sandbox.my.salesforce.com/*",
     "https://bcministryofhealth-environmentalh2--demo.sandbox.my.salesforce.com/*",
     "https://bcministryofhealth-environmentalh2--uat.sandbox.my.salesforce.com/*",
-    "https://bcministryofhealth-environmentalh2--dat.sandbox.my.salesforce.com/*"
+    "https://bcministryofhealth-environmentalh2--dat.sandbox.my.salesforce.com/*",
+    "https://bcministryofhealth-environmentalh2--opsqa.sandbox.my.salesforce.com/*",
+    "https://bcministryofhealth-environmentalh2--opsdev.sandbox.my.salesforce.com/*",
   ]
   web_origins = [
   ]
